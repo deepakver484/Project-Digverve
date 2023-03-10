1d1fe09138ded6b74cd6e49530d63ba78eb2af61
Deepak verma <deepakkumarverma484@gmail.com>
2023-03-10 17:34:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, flash
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+app.secret_key = 'mysecretkey'
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        df = pd.DataFrame(data, index= [0])
+        df  = df.astype('int64')
+        with open('normalizer_pkl' , 'rb') as f:
+            norm = pickle.load(f)
+        with open('model_pkl' , 'rb') as f:
+            model = pickle.load(f)
+        y_pred = model.predict(norm.transform(df))[0]
+        if y_pred == 0:
+            message = 'This client will not subscribe term deposit' 
+        else:
+            message = 'This client will subscribe term deposit'
+        flash(message)
+        return render_template('index.html')
+    else:
+        return render_template('index.html') 
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+    
